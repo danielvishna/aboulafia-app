33fcd821baf8f60577a052fe4832df2e47cd5047
daniel.vishna <daniel.vishna@gmail.com>
2025-10-23 13:53:09 +0300

    try to change the location of the css

--- aboulafia/hooks.py
+++ aboulafia/hooks.py
@@ -25,10 +25,9 @@ doc_events = {
         "validate": "aboulafia.aboulafia.doctype.customer.customer.on_validate",
     },
 }
-app_include_css = "/assets/aboulafia/css/custom_styles.css"
-app_include_js = [
-    "/assets/aboulafia/js/custom_ui.js",
-]
+app_include_css = ["/assets/aboulafia/css/custom_styles.css"]
+
+app_include_js = ["/assets/aboulafia/js/custom_ui.js",]
 
 # app_include_js = "public/js/_ping.js"
 
